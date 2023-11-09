61da8733fa3b2b229a5c49a2cd0466a38d5e7c04
(Student  C00287822) Shane Moran <C00287822@setu.ie>
2023-11-09 11:49:36 +0000

    even numbers

--- evenPractice/evenPractice/even.cpp
+++ evenPractice/evenPractice/even.cpp
@@ -2,8 +2,19 @@
 //9/11/23
 //even
 
+#include <iostream>
+
 int main()
 {
+	//Declare variables
+	int number = 0;
+	std::cout << "Please enter your number - ";
+	std::cin >> number;
+
+	if (number % 2 == 0)
+	{
+		std::cout << "Your number is even";
+	}
 
 	return 0;
 }
