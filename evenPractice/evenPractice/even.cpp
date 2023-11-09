0fb8a5e0ac73fd1315322d87d735b5de7a89d64d
(Student  C00287822) Shane Moran <C00287822@setu.ie>
2023-11-09 12:13:02 +0000

    second odd numbers commit

--- evenPractice/evenPractice/even.cpp
+++ evenPractice/evenPractice/even.cpp
@@ -1,9 +1,18 @@
 //Shane Moran
 //9/11/23
 //even
+#include<iostream>
 
 int main()
 {
+	//Declare variables
+	int number = 0;
+	std::cout << "Please enter your number - ";
+	std::cin >> number;
 
+	if (number % 2 == 1)
+	{
+		std::cout << "Your number is odd" << std::endl;
+	}
 	return 0;
 }
